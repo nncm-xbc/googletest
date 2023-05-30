a6ec81c1deb35734684c8adcdc6d77a9892ffd30
Marco Scarpelli <underscore.marczero@gmail.com>
2023-05-30 11:51:00 +0200

    Added test flags

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,5 +2,5 @@
 rm -Rf build # No error message if non-existent
 mkdir build
 cd build
-cmake ..
+cmake .. -Dgmock_build_tests=ON -Dgtest_build_tests=ON
 make -j$(nproc) # Make with amount of threads
