8c338f9f5060a38e200c88181e6e3fee2cfc2199
nncm-xbc <simon.hugot.67@gmail.com>
2023-06-01 12:54:14 +0200

    nvm

--- Scripts/docker.sh
+++ Scripts/docker.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Install and initialize docker
-apt update
-apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-systemctl start docker
-systemctl status docker
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+sudo systemctl status docker
 
 docker build --pull --rm -f "Dockerfile" -t googletest:latest "." 
