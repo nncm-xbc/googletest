e98986bac6ae1c3147c51770f9a7f92c5bd8bbb9
nncm-xbc <simon.hugot.67@gmail.com>
2023-06-01 12:06:58 +0200

    forgot to install docker on EC2

--- Scripts/docker.sh
+++ Scripts/docker.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+
+# Install and initialize docker
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+sudo systemctl status docker
+
 docker build --pull --rm -f "Dockerfile" -t googletest:latest "." 
