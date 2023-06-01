96ae28fc55dba9b125231ba441bfafe13e2b01e8
nncm-xbc <simon.hugot.67@gmail.com>
2023-06-01 13:03:37 +0200

    yet again

--- Scripts/docker.sh
+++ Scripts/docker.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Install and initialize docker
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
 sudo systemctl status docker
 
